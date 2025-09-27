2a0d22f9872406725229686b36a2d2bb91449b9b
sujin209 <20201036@sungshin.ac.kr>
2025-09-27 12:20:01 +0900

    [Feat] 마이페이지 레이아웃 틀 제작

--- src/app/(stack)/my-page/page.tsx
+++ src/app/(stack)/my-page/page.tsx
@@ -1,4 +1,15 @@
+import Button from "@/shared/components/Button";
+
 function Page() {
-  return <div>myPage</div>;
+  return (
+    <div className="flex-1 p-10 flex-center flex-col gap-8">
+      <div className="w-4/5 flex-center flex-col">사용자 정보</div>
+      <Button fullWidth>이메일 설정</Button>
+      <Button fullWidth>비밀번호 바꾸기</Button>
+      <Button fullWidth color="darkgreen">
+        회원탈퇴하기
+      </Button>
+    </div>
+  );
 }
 export default Page;
