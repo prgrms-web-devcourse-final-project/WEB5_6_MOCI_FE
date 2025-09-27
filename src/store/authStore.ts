40ffbb596a2f11cbcbe061849371d9dde57e7f78
sujin209 <20201036@sungshin.ac.kr>
2025-09-28 00:02:39 +0900

    [Feat] 로그인상태관리 수정, AuthProvider 추가

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -14,11 +14,33 @@ type User = {
 interface AuthState {
   user: User | null;
   setUser: (user: User) => void;
+  fetchUser: () => Promise<void>;
   logout: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
   setUser: (user: User) => set({ user }),
+  fetchUser: async () => {
+    const userId = localStorage.getItem("userId");
+    if (!userId) set({ user: null });
+    else {
+      const res = await fetch(`http://localhost:8080/api/v1/users/${userId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json; charset: UTF-8",
+        },
+        credentials: "include",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+        set({ user: data.user });
+      } else {
+        set({ user: null });
+        localStorage.removeItem("userId");
+      }
+    }
+  },
   logout: () => set({ user: null }),
 }));
