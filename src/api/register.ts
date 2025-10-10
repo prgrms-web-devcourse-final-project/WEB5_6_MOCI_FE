d45aba6e3c220c3ec7b18922b96f780032eb2381
Young-mang <fgb9763@gmail.com>
2025-10-11 01:58:24 +0900

    [Fix] 오타수정

--- src/api/register.ts
+++ src/api/register.ts
@@ -5,7 +5,7 @@ export const register = async (data: {
   phone: string;
   password: string;
 }) => {
-  const res = await fetch(`${BASE_URL}api/v1/auth/register`, {
+  const res = await fetch(`${BASE_URL}/api/v1/auth/register`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
