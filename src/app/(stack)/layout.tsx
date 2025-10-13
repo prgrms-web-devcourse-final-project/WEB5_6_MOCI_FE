6f0b1c309c324c939c3e26bea3a1a598a6cbd391
Young-mang <fgb9763@gmail.com>
2025-10-13 10:28:39 +0900

    [Fix] 자료 상세 페이지 스크롤 고정 문제 해결

--- src/app/(stack)/layout.tsx
+++ src/app/(stack)/layout.tsx
@@ -2,10 +2,10 @@ import StackHeader from "@/shared/components/StackHeader";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <>
       <StackHeader />
-      <main className="root-min-h">{children}</main>
-    </div>
+      <main className="root-min-h h-dvh flex flex-col flex-1">{children}</main>
+    </>
   );
 }
 
